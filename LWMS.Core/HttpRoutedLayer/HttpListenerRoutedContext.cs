24e004397fa3476e290c3a99c9c303f528aef502
Creeper Lv <creeperlv38@outlook.com>
2020-10-16 11:31:26 +0800

    RoutedPipelineStream is now almost completed.

--- LWMS.Core/HttpRoutedLayer/HttpListenerRoutedContext.cs
+++ LWMS.Core/HttpRoutedLayer/HttpListenerRoutedContext.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using CLUNL;
+using CLUNL.Pipeline;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Net;
 using System.Net.WebSockets;
 using System.Security.Principal;
@@ -10,7 +13,7 @@ namespace LWMS.Core.HttpRoutedLayer
 {
     public class HttpListenerRoutedContext
     {
-        HttpListenerContext CoreContext;
+        internal HttpListenerContext CoreContext;
         public HttpListenerRequest Request { get; private set; }
         public HttpListenerRoutedResponse Response { get; private set; }
         public IPrincipal User { get; private set; }
@@ -19,7 +22,7 @@ namespace LWMS.Core.HttpRoutedLayer
             CoreContext = Context;
             Request = CoreContext.Request;
             User = CoreContext.User;
-            Response = new HttpListenerRoutedResponse(CoreContext.Response);
+            Response = new HttpListenerRoutedResponse(this);
 
         }
         public async Task<HttpListenerWebSocketContext> AcceptWebSocketAsync(string subProtocol)
@@ -42,6 +45,8 @@ namespace LWMS.Core.HttpRoutedLayer
     public class HttpListenerRoutedResponse : IDisposable
     {
         HttpListenerResponse CoreResponse;
+        HttpListenerRoutedContext context;
+        public RoutedPipelineStream OutputStream;
         public int StatusCode { get => CoreResponse.StatusCode; set => CoreResponse.StatusCode = value; }
         public long ContentLength64 { get => CoreResponse.ContentLength64; set => CoreResponse.ContentLength64 = value; }
         public string ContentType { get => CoreResponse.ContentType; set => CoreResponse.ContentType = value; }
@@ -53,9 +58,11 @@ namespace LWMS.Core.HttpRoutedLayer
         public string RedirectLocation { get => CoreResponse.RedirectLocation; set => CoreResponse.RedirectLocation = value; }
         public bool SendChunked { get => CoreResponse.SendChunked; set => CoreResponse.SendChunked = value; }
         public string StatusDescription { get => CoreResponse.StatusDescription; set => CoreResponse.StatusDescription = value; }
-        public HttpListenerRoutedResponse(HttpListenerResponse response)
+        public HttpListenerRoutedResponse(HttpListenerRoutedContext context)
         {
-            CoreResponse = response;
+            this.context = context;
+            CoreResponse = this.context.CoreContext.Response;
+            OutputStream = new RoutedPipelineStream(CoreResponse.OutputStream,context);
         }
         public void Abort()
         {
@@ -90,9 +97,184 @@ namespace LWMS.Core.HttpRoutedLayer
         {
             CoreResponse.Redirect(url);
         }
-        public void SetCookie(Cookie cookie) { 
+        public void SetCookie(Cookie cookie)
+        {
             CoreResponse.SetCookie(cookie);
         }
+    }
+    public class RoutedPipelineStream : Stream
+    {
+        public static PipelineStreamProcessor DefaultPublicStreamProcessor = new PipelineStreamProcessor();
+        Stream CoreStream;
+        HttpListenerRoutedContext CoreContext;
+        public RoutedPipelineStream(Stream stream,HttpListenerRoutedContext Core)
+        {
+            CoreStream = stream;
+        }
+        public override bool CanRead => CoreStream.CanRead;
 
+        public override bool CanSeek => CoreStream.CanSeek;
+
+        public override bool CanWrite => CoreStream.CanWrite;
+
+        public override long Length => CoreStream.Length;
+
+        public override long Position { get => CoreStream.Position; set => CoreStream.Position = value; }
+
+        public override void Flush()
+        {
+            DefaultPublicStreamProcessor.Process(new PipelineData(CoreStream, null, new RoutedPipelineStreamOptions() { Method = RoutedPipelineStreamMethod.FLUSH, Context=CoreContext }));
+        }
+
+        public override int Read(byte[] buffer, int offset, int count)
+        {
+            int Result = 0;
+            ReadArguments _result = (ReadArguments)DefaultPublicStreamProcessor.Process(new PipelineData(CoreStream,new ReadArguments(buffer, offset, count,Result), new RoutedPipelineStreamOptions() { Method = RoutedPipelineStreamMethod.READ, Context = CoreContext })).SecondaryData;
+            return _result.Result;
+        }
+
+        public override long Seek(long offset, SeekOrigin origin)
+        {
+            long Result=0;
+            var _result=(SeekArguments)DefaultPublicStreamProcessor.Process(new PipelineData(CoreStream,new SeekArguments(offset,origin,Result), new RoutedPipelineStreamOptions() { Method = RoutedPipelineStreamMethod.SETLENGTH, Context = CoreContext })).SecondaryData;
+            return _result.Result;
+        }
+
+        public override void SetLength(long value)
+        {
+            DefaultPublicStreamProcessor.Process(new PipelineData(CoreStream, value, new RoutedPipelineStreamOptions() { Method = RoutedPipelineStreamMethod.SETLENGTH, Context = CoreContext }));
+        }
+
+        public override void Write(byte[] buffer, int offset, int count)
+        {
+            DefaultPublicStreamProcessor.Process(new PipelineData(CoreStream,new WriteArguments(buffer, offset, count), new RoutedPipelineStreamOptions() { Method = RoutedPipelineStreamMethod.WRITE, Context = CoreContext }));
+        }
+    }
+    public class SeekArguments
+    {
+        public long Offset;
+        public SeekOrigin Origin;
+        public long Result;
+        public SeekArguments(long offset,SeekOrigin origin,long result)
+        {
+            Offset = offset;
+            Origin = origin;
+            Result = result;
+        }
+    }
+    public class ReadArguments
+    {
+        public byte[] Buffer;
+        public int Offset;
+        public int Count;
+        public int Result;
+        public ReadArguments(byte[] buffer,int offset,int count,int result)
+        {
+            Buffer = buffer;
+            Offset = offset;
+            Count = count;
+            Result = result;
+        }
+    }
+    public class WriteArguments
+    {
+        public byte[] Buffer;
+        public int Offset;
+        public int Count;
+        public WriteArguments(byte[] buffer,int offset,int count)
+        {
+            Buffer = buffer;
+            Offset = offset;
+            Count = count;
+        }
+    }
+    public class PipelineStreamProcessor : IPipelineProcessor
+    {
+        List<IPipedProcessUnit> processUnits = new List<IPipedProcessUnit>();
+        public void Init(params IPipedProcessUnit[] units)
+        {
+            if (units.Length == 0)
+            {
+                processUnits.Add(new DefaultProcessUnit());
+            }
+            else
+            {
+                processUnits = new List<IPipedProcessUnit>(units);
+            }
+        }
+        public void Init()
+        {
+            Init(new IPipedProcessUnit[0]);
+        }
+
+        public void Init(ProcessUnitManifest manifest)
+        {
+            processUnits = manifest.GetUnitInstances();
+        }
+        public PipelineData Process(PipelineData Input)
+        {
+            bool willIgnore = false;
+            try
+            {
+                if (LibraryInfo.GetFlag(FeatureFlags.Pipeline_IgnoreError) == 1)
+                {
+                    willIgnore = true;
+                }
+            }
+            catch (Exception)
+            {
+                //Ignore
+            }
+            return Process(Input, willIgnore);
+        }
+
+        public PipelineData Process(PipelineData Input, bool IgnoreError)
+        {
+            if (IgnoreError)
+            {
+
+                foreach (var item in processUnits)
+                {
+                    try
+                    {
+                        var output = item.Process(Input);
+                        if (Input.CheckContinuity(output))
+                        {
+                            Input = output;
+                        }
+                    }
+                    catch (Exception)
+                    {
+                    }
+                }
+                return Input;
+            }
+            else
+            {
+
+                foreach (var item in processUnits)
+                {
+                    var output = item.Process(Input);
+                    if (Input.CheckContinuity(output))
+                    {
+                        Input = output;
+                    }
+                    else
+                    {
+                        throw new PipelineDataContinuityException(item);
+                    }
+                }
+                return Input;
+            }
+        }
+    }
+    public class RoutedPipelineStreamOptions
+    {
+        public HttpListenerRoutedContext Context { get; internal set; }
+        public RoutedPipelineStreamMethod Method { get; internal set; }
+    }
+    public enum RoutedPipelineStreamMethod
+    {
+        READ, SEEK, SETLENGTH, WRITE,FLUSH
     }
 }
