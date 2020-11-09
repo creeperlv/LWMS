ca2aef5ab0793e1f70c2b2f6ab10bfd5bb3bc146
Creeper Lv <creeperlv38@outlook.com>
2020-11-09 14:21:34 +0800

    ConsoleCmdOutUnit now associated with LWMSTraceListener.EnableConsoleOutput and LWMSTraceListener.BeautifyConsoleOutput.

--- LWMS.Management/ConsoleCmdOutUnit.cs
+++ LWMS.Management/ConsoleCmdOutUnit.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 using CLUNL.Pipeline;
+using LWMS.Core.Log;
 using LWMS.Core.WR;
 
 namespace LWMS.Management
@@ -13,27 +14,36 @@ namespace LWMS.Management
     {
         public PipelineData Process(PipelineData Input)
         {
-            PipedRoutedWROption option = (PipedRoutedWROption)Input.Options;
-            if(option.PipedRoutedWROperation== PipedRoutedWROperation.WRITE)
+            if (LWMSTraceListener.EnableConsoleOutput == true)
             {
-                Console.Out.Write((string)Input.PrimaryData);
-            }else if(option.PipedRoutedWROperation== PipedRoutedWROperation.WRITELINE)
-            {
-                Console.Out.WriteLine((string)Input.PrimaryData);
-            }else if(option.PipedRoutedWROperation== PipedRoutedWROperation.FLUSH)
-            {
-                Console.Out.Flush();
-            }else if(option.PipedRoutedWROperation== PipedRoutedWROperation.FGCOLOR)
-            {
-                Console.ForegroundColor = (ConsoleColor)Input.PrimaryData;
-            }
-            else if (option.PipedRoutedWROperation == PipedRoutedWROperation.BGCOLOR)
-            {
-                Console.BackgroundColor = (ConsoleColor)Input.PrimaryData;
-            }
-            else if (option.PipedRoutedWROperation == PipedRoutedWROperation.RESETCOLOR)
-            {
-                Console.ResetColor();
+                PipedRoutedWROption option = (PipedRoutedWROption)Input.Options;
+                if (option.PipedRoutedWROperation == PipedRoutedWROperation.WRITE)
+                {
+                    Console.Out.Write((string)Input.PrimaryData);
+                }
+                else if (option.PipedRoutedWROperation == PipedRoutedWROperation.WRITELINE)
+                {
+                    Console.Out.WriteLine((string)Input.PrimaryData);
+                }
+                else if (option.PipedRoutedWROperation == PipedRoutedWROperation.FLUSH)
+                {
+                    Console.Out.Flush();
+                }
+                else if (LWMSTraceListener.BeautifyConsoleOutput == true)
+                {
+                    if (option.PipedRoutedWROperation == PipedRoutedWROperation.FGCOLOR)
+                    {
+                        Console.ForegroundColor = (ConsoleColor)Input.PrimaryData;
+                    }
+                    else if (option.PipedRoutedWROperation == PipedRoutedWROperation.BGCOLOR)
+                    {
+                        Console.BackgroundColor = (ConsoleColor)Input.PrimaryData;
+                    }
+                    else if (option.PipedRoutedWROperation == PipedRoutedWROperation.RESETCOLOR)
+                    {
+                        Console.ResetColor();
+                    }
+                }
             }
             return Input;
         }
