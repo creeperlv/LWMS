b537000d92a0d61f0349a979bf90f372d5adee8c
Creeper Lv <creeperlv38@outlook.com>
2021-02-18 02:21:17 +0800

    Working on an implementation of manager of Side-By-Side assemblies.

---
+++ LWMS.Core.SBSDomain/DomainManager.cs
@@ -0,0 +1,49 @@
+﻿using LWMS.Core.Authentication;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+
+namespace LWMS.Core.SBSDomain
+{
+    public class DomainManager
+    {
+        internal static string TrustedInstaller = null;
+        public static void SetTrustedInstaller(string Auth)
+        {
+            if (TrustedInstaller is null)
+            {
+                TrustedInstaller = Auth;
+            }
+        }
+        static Dictionary<string, List<Assembly>> DLLs = new Dictionary<string, List<Assembly>>();
+        public static Assembly LoadFromFile(string Auth, string PathToDLL)
+        {
+            Assembly v = null;
+            OperatorAuthentication.AuthedAction(Auth, () =>
+            {
+                var guid = Guid.NewGuid();
+                var temp = Path.Combine(Path.GetTempPath(), guid.ToString());
+                FileInfo fi = new(PathToDLL);
+                Directory.CreateDirectory(temp);
+                var Final = fi.CopyTo(Path.Combine(temp + fi.Name));
+                v = Assembly.LoadFrom(Final.FullName);
+                if (!DLLs.ContainsKey(fi.Name))
+                {
+                    DLLs.Add(fi.Name, new());
+                }
+                DLLs[fi.Name].Add(v);
+            }, false, true, PermissionID.Core_SBS_Load, PermissionID.Core_SBS_All);
+            return v;
+        }
+        public static Assembly GetAssembly(string Auth, string Name, int Version = 0)
+        {
+            Assembly v = null;
+            OperatorAuthentication.AuthedAction(Auth, () =>
+            {
+                v = DLLs[Name][Version];
+            }, false, true, PermissionID.Core_SBS_Read, PermissionID.Core_SBS_All);
+            return v;
+        }
+    }
+}
