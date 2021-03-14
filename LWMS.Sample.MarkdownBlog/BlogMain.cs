040996a9045db1eb44fd77108dba2a41daf6245f
Creeper Lv <creeperlv38@outlook.com>
2021-03-14 20:37:01 +0800

    BlogMain will no longer throws an exception if it cannot find Markdig.dll, instead, it will log a message. Fixed a text error in index.html.

--- LWMS.Sample.MarkdownBlog/BlogMain.cs
+++ LWMS.Sample.MarkdownBlog/BlogMain.cs
@@ -14,9 +14,18 @@ namespace LWMS.Sample.MarkdownBlog
 {
     public class BlogMain : IPipedProcessUnit
     {
+        bool isMarkdownUnavailable = false;
         public BlogMain()
         {
-            Assembly.LoadFrom("Markdig.dll");
+            try
+            {
+                Assembly.LoadFrom("Markdig.dll");
+            }
+            catch (Exception)
+            {
+                isMarkdownUnavailable = true;
+                Trace.WriteLine("Cannot load Markdig.dll, MDBlog may be unable to work correctly.");
+            }
             //In case that dependency is not loaded.
         }
         StorageFile ArticleTemplate = null;
@@ -25,69 +34,78 @@ namespace LWMS.Sample.MarkdownBlog
         StorageFolder Articles = null;
         public PipelineData Process(PipelineData Input)
         {
-            if (ArticleTemplate is null)
-                ArticleTemplate = ApplicationStorage.CurrentModule.GetFile("Template.html");
-            if (ArticleListTemplate is null)
-                ArticleListTemplate = ApplicationStorage.CurrentModule.GetFile("ArticleListTemplate.html");
-            if (ArticleListItemTemplate is null)
-                ArticleListItemTemplate = ApplicationStorage.CurrentModule.GetFile("ArticleListItemTemplate.html");
-            if (Articles is null)
-                Articles = ApplicationStorage.CurrentModule.CreateFolder("Articles", true);
-            HttpListenerRoutedContext context = Input.PrimaryData as HttpListenerRoutedContext;
-            var path0 = context.Request.Url.LocalPath.Substring(1);
-            if (path0.ToUpper().StartsWith("BLOGS"))
+            if (isMarkdownUnavailable is false)
             {
-                var path1 = path0.Substring(path0.IndexOf("/") + 1);
-                StorageFile f;
-                if (path1.ToUpper().EndsWith(".INFO"))
+                if (ArticleTemplate is null)
+                    ArticleTemplate = ApplicationStorage.CurrentModule.GetFile("Template.html");
+                if (ArticleListTemplate is null)
+                    ArticleListTemplate = ApplicationStorage.CurrentModule.GetFile("ArticleListTemplate.html");
+                if (ArticleListItemTemplate is null)
+                    ArticleListItemTemplate = ApplicationStorage.CurrentModule.GetFile("ArticleListItemTemplate.html");
+                if (Articles is null)
+                    Articles = ApplicationStorage.CurrentModule.CreateFolder("Articles", true);
+                HttpListenerRoutedContext context = Input.PrimaryData as HttpListenerRoutedContext;
+                var path0 = context.Request.Url.LocalPath.Substring(1);
+                if (path0.ToUpper().StartsWith("BLOGS"))
                 {
-
-                }
-                else
-                {
-                    if (Articles.GetFile(path1, out f, false))
+                    var path1 = path0.Substring(path0.IndexOf("/") + 1);
+                    StorageFile f;
+                    if (path1.ToUpper().EndsWith(".INFO"))
                     {
-                        Trace.WriteLine("MDBlog>>Article:"+f.Name);
-                        var MDContnet = Markdown.ToHtml(File.ReadAllText(f.ItemPath));
-                        string Title = f.Name;
-                        StorageFile info;
-                        if (Articles.GetFile(path1 + ".info", out info, false))
-                        {
-                            var infos = File.ReadAllLines(info.ItemPath);
-                            if (infos.Length > 0)
-                                Title = infos[0];
-                        }
 
-                        var FinalContent = File.ReadAllText(ArticleTemplate.ItemPath).Replace("%Content%", MDContnet).Replace("%Title%", Title);
+                    }
+                    else
+                    {
+                        if (Articles.GetFile(path1, out f, false))
+                        {
+                            Trace.WriteLine("MDBlog>>Article:" + f.Name);
+                            var MDContnet = Markdown.ToHtml(File.ReadAllText(f.ItemPath));
+                            string Title = f.Name;
+                            StorageFile info;
+                            if (Articles.GetFile(path1 + ".info", out info, false))
+                            {
+                                var infos = File.ReadAllLines(info.ItemPath);
+                                if (infos.Length > 0)
+                                    Title = infos[0];
+                            }
+
+                            var FinalContent = File.ReadAllText(ArticleTemplate.ItemPath).Replace("%Content%", MDContnet).Replace("%Title%", Title);
+                            context.Response.OutputStream.Write(Encoding.UTF8.GetBytes(FinalContent));
+                            (Input.SecondaryData as HttpPipelineArguments).isHandled = true;
+                            return Input;
+                        }
+                    }
+                    {
+                        Trace.WriteLine("MDBlog>>MainPage");
+                        var list = Articles.GetFiles();
+
+                        var MainContent = File.ReadAllText(ArticleListTemplate.ItemPath);
+                        var ItemTemplate = File.ReadAllText(ArticleListItemTemplate.ItemPath);
+                        var ItemList = "";
+                        foreach (var item in list)
+                        {
+                            if (item.Name.ToUpper().EndsWith(".INFO"))
+                            {
+                                var infos = File.ReadAllLines(item.ItemPath);
+                                if (infos.Length > 0)
+                                    ItemList += ItemTemplate.Replace("%Title%", infos[0]).Replace("%Link%", "./Blogs/" + item.Name.Substring(0, item.Name.Length - 5));
+                            }
+                        }
+                        var FinalContent = MainContent.Replace("%Content%", ItemList).Replace("%Title%", ApplicationConfiguration.Current.GetValue("BlogTitle", "LWMS Blog"));
                         context.Response.OutputStream.Write(Encoding.UTF8.GetBytes(FinalContent));
                         (Input.SecondaryData as HttpPipelineArguments).isHandled = true;
                         return Input;
                     }
                 }
-                {
-                    Trace.WriteLine("MDBlog>>MainPage");
-                    var list = Articles.GetFiles();
-
-                    var MainContent = File.ReadAllText(ArticleListTemplate.ItemPath);
-                    var ItemTemplate = File.ReadAllText(ArticleListItemTemplate.ItemPath);
-                    var ItemList = "";
-                    foreach (var item in list)
-                    {
-                        if (item.Name.ToUpper().EndsWith(".INFO"))
-                        {
-                            var infos = File.ReadAllLines(item.ItemPath);
-                            if (infos.Length > 0)
-                                ItemList += ItemTemplate.Replace("%Title%", infos[0]).Replace("%Link%", "./Blogs/"+item.Name.Substring(0, item.Name.Length - 5));
-                        }
-                    }
-                    var FinalContent = MainContent.Replace("%Content%", ItemList).Replace("%Title%", ApplicationConfiguration.Current.GetValue("BlogTitle", "LWMS Blog"));
-                    context.Response.OutputStream.Write(Encoding.UTF8.GetBytes(FinalContent));
-                    (Input.SecondaryData as HttpPipelineArguments).isHandled = true;
-                    return Input;
-                }
-            }
                 (Input.SecondaryData as HttpPipelineArguments).isHandled = false;
-            return Input;
+                return Input;
+
+            }
+            else
+            {
+                (Input.SecondaryData as HttpPipelineArguments).isHandled = false;
+                return Input;
+            }
         }
     }
 }
