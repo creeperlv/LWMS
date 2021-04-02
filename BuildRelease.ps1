2a19d25cb6cbeb1a831cfa6ba8102b4b1f3ccaa4
Creeper Lv <creeperlv38@outlook.com>
2021-04-02 13:39:37 +0800

    Completed build script for Release configuration.

--- BuildRelease.ps1
+++ BuildRelease.ps1
@@ -1,5 +1,33 @@
-dotnet build -c Release
-cd LWMS.Management.Commands
-dotnet build -c Release
-Copy-Item ".\bin\Release\net5.0\LWMS.Management.Commands.dll" -Destination "..\LWMS\bin\Release\net5.0\LWMS.Management.Commands.dll" -Force
+$Arch="win-x64"
+if($args.Count -gt 0){
+$Arch=$args[0]
+}
+rm ".\LWMS\bin\Release\" -Force -Confirm -Recurse
+# Build projects
+cd LWMS
+dotnet build -c Release -r $Arch
 cd ..
+#  LWMS.Management.Commands
+cd LWMS.Management.Commands
+dotnet build -c Release -r $Arch
+Copy-Item ".\bin\Release\net5.0\$Arch\LWMS.Management.Commands.dll" -Destination "..\LWMS\bin\Release\net5.0\$Arch\LWMS.Management.Commands.dll" -Force
+cd ..
+#  LWMS.EventDrivenRequests
+cd LWMS.EventDrivenRequests
+dotnet build -c Release -r $Arch
+Copy-Item ".\bin\Release\net5.0\$Arch\LWMS.EventDrivenRequests.dll" -Destination "..\LWMS\bin\Release\net5.0\$Arch\LWMS.EventDrivenRequests.dll" -Force
+cd ..
+#  LWMS.SimpleDirectoryBrowser
+cd LWMS.SimpleDirectoryBrowser
+dotnet build -c Release -r $Arch
+Copy-Item ".\bin\Release\net5.0\$Arch\LWMS.SimpleDirectoryBrowser.dll" -Destination "..\LWMS\bin\Release\net5.0\$Arch\LWMS.SimpleDirectoryBrowser.dll" -Force
+cd ..
+#  LWMS.Sample.MarkdownBlog
+cd LWMS.Sample.MarkdownBlog
+dotnet build -c Release -r $Arch
+Copy-Item ".\bin\Release\net5.0\$Arch\LWMS.Sample.MarkdownBlog.dll" -Destination "..\LWMS\bin\Release\net5.0\$Arch\LWMS.Sample.MarkdownBlog.dll" -Force
+cd ..
+# Pack the binary files
+cd ".\LWMS\bin\Release\"
+Compress-Archive ".\net5.0\$Arch\" ".\LWMS.$Arch.zip"
+cd ..\..\..\
