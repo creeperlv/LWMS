2798f78cf4b9b013be820a8a22efcb677084e961
Creeper Lv <creeperlv38@outlook.com>
2021-04-03 02:22:29 +0800

    Moved the actual method that invokes tasks from anonymous method to a static named method to make logger log the right callee method.

--- LWMS.Core/ScheduledTask/IScheduleTask.cs
+++ LWMS.Core/ScheduledTask/IScheduleTask.cs
@@ -86,69 +86,71 @@ namespace LWMS.Core.ScheduledTask
     {
         internal static bool Stop = false;
         internal static bool Pause = false;
+        async static void Run()
+        {
+
+            int step = 0;
+            while (Stop is false)
+            {
+                if (Pause is false)
+                {
+
+                    foreach (var item in ScheduleTask.ScheduledTasks)
+                    {
+                        bool willExecute = false;
+                        switch (item.Value.gap)
+                        {
+                            case ScheduleTaskGap.Sec5:
+                                willExecute = step % 1 == 0;
+                                break;
+                            case ScheduleTaskGap.Sec10:
+                                willExecute = step % 2 == 0;
+                                break;
+                            case ScheduleTaskGap.Sec30:
+                                willExecute = step % 6 == 0;
+                                break;
+                            case ScheduleTaskGap.Min1:
+                                willExecute = step % 12 == 0;
+                                break;
+                            case ScheduleTaskGap.Min5:
+                                willExecute = step % 60 == 0;
+                                break;
+                            case ScheduleTaskGap.Min15:
+                                willExecute = step % 180 == 0;
+                                break;
+                            case ScheduleTaskGap.Min30:
+                                willExecute = step % 360 == 0;
+                                break;
+                            case ScheduleTaskGap.Hour1:
+                                willExecute = step % 720 == 0;
+                                break;
+                            case ScheduleTaskGap.Hour6:
+                                willExecute = step % 4320 == 0;
+                                break;
+                            case ScheduleTaskGap.Hour12:
+                                willExecute = step % 8640 == 0;
+                                break;
+                            case ScheduleTaskGap.Day1:
+                                willExecute = step % 17280 == 0;
+                                break;
+                            default:
+                                break;
+                        }
+                        if (willExecute is true)
+                        {
+                            (item.Value.RealTask.TargetObject as IScheduleTask).Task();
+                            Trace.WriteLine(Language.Query("LWMS.ScheduleTask.Run", "Task {0} has been invoked on schedule.", item.Key));
+                        }
+                    }
+                    if (step % 17280 == 0) step = 0;
+                    step++;
+                }
+                await Task.Delay(5000);
+            }
+        }
         internal static void StartRun()
         {
-            Task.Run(async () =>
-            {
-                int step = 0;
-                while (Stop is false)
-                {
-                    if (Pause is false)
-                    {
-
-                        foreach (var item in ScheduleTask.ScheduledTasks)
-                        {
-                            bool willExecute = false;
-                            switch (item.Value.gap)
-                            {
-                                case ScheduleTaskGap.Sec5:
-                                    willExecute = step % 1 == 0;
-                                    break;
-                                case ScheduleTaskGap.Sec10:
-                                    willExecute = step % 2 == 0;
-                                    break;
-                                case ScheduleTaskGap.Sec30:
-                                    willExecute = step % 6 == 0;
-                                    break;
-                                case ScheduleTaskGap.Min1:
-                                    willExecute = step % 12 == 0;
-                                    break;
-                                case ScheduleTaskGap.Min5:
-                                    willExecute = step % 60 == 0;
-                                    break;
-                                case ScheduleTaskGap.Min15:
-                                    willExecute = step % 180 == 0;
-                                    break;
-                                case ScheduleTaskGap.Min30:
-                                    willExecute = step % 360 == 0;
-                                    break;
-                                case ScheduleTaskGap.Hour1:
-                                    willExecute = step % 720 == 0;
-                                    break;
-                                case ScheduleTaskGap.Hour6:
-                                    willExecute = step % 4320 == 0;
-                                    break;
-                                case ScheduleTaskGap.Hour12:
-                                    willExecute = step % 8640 == 0;
-                                    break;
-                                case ScheduleTaskGap.Day1:
-                                    willExecute = step % 17280 == 0;
-                                    break;
-                                default:
-                                    break;
-                            }
-                            if (willExecute is true)
-                            {
-                                (item.Value.RealTask.TargetObject as IScheduleTask).Task();
-                                Trace.WriteLine(Language.Query("LWMS.ScheduleTask.Run", "Task {0} has been invoked on schedule.", item.Key));
-                            }
-                        }
-                        if (step % 17280 == 0) step = 0;
-                        step++;
-                    }
-                    await Task.Delay(5000);
-                }
-            });
+            Task.Run(Run);
         }
     }
     public enum ScheduleTaskGap
