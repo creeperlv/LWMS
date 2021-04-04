96a07e525b114865b2bd0c5c0f9196fe03daca97
Creeper Lv <creeperlv38@outlook.com>
2021-04-04 14:06:38 +0800

    Continue work on the Remote Shell.

--- LWMS.Core.RemoteShell.Server/RSServer.cs
+++ LWMS.Core.RemoteShell.Server/RSServer.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Net;
 using System.Net.Sockets;
+using System.Threading;
+using System.Threading.Tasks;
 using CLUNL.Pipeline;
 using LWMS.Core.WR;
 
@@ -8,18 +10,56 @@ namespace LWMS.Core.RemoteShell.Server
 {
     public class RSServer
     {
+        int MaxConnections;
+        int BufferSize;
+        //BufferManager
+        //SocketAsyncEventArgsPool eventArgsPool;
         public static string RSAPublicKey;
         public static string RSAPrivateKey;
         public Socket Listener;
-        public RSServer(IPEndPoint point)
+        Semaphore ThreadLimitation;
+        public RSServer(IPEndPoint point,int MaxConnections,int BufferSize)
         {
             Listener = new Socket(point.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
             Listener.Bind(point);
+            this.MaxConnections = MaxConnections;
+            this.BufferSize = BufferSize;
+            ThreadLimitation = new Semaphore(MaxConnections, MaxConnections);
+        }
+        bool Stop = false;
+        public void Start()
+        {
+            Listener.Listen(100);
+            Task.Run(() => {
+                while (Stop is false)
+                {
+                    var s=Listener.Accept();
+                    var v = ValidAndLogin(s);
+                    if (v.Item1)
+                    {
+                        ThreadLimitation.WaitOne();
+                        (string, Socket) _s=(v.Item2,s);
+                        RSCMDReciver.AddSocket(_s);
+                    }
+                }
+            });
+        }
+        internal void Watch((string, Socket) s)
+        {
+            s.Item2.ReceiveTimeout =0;
+        }
+        internal (bool,string) ValidAndLogin(Socket client)
+        {
+            return (false,"");
         }
     }
+
     public class RSCMDReciver : IPipedProcessUnit
     {
+        internal static void AddSocket((string,Socket) s)
+        {
 
+        }
         public PipelineData Process(PipelineData Input)
         {
             {
