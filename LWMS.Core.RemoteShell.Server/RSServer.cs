385bb9be1b1993c10d50172855145af3fd62cc65
Creeper Lv <creeperlv38@outlook.com>
2021-04-04 19:26:38 +0800

    AESLayer should work now.

--- LWMS.Core.RemoteShell.Server/RSServer.cs
+++ LWMS.Core.RemoteShell.Server/RSServer.cs
@@ -1,9 +1,13 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Net;
 using System.Net.Sockets;
+using System.Security.Cryptography;
+using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using CLUNL.Pipeline;
+using LWMS.Core.Authentication;
 using LWMS.Core.WR;
 
 namespace LWMS.Core.RemoteShell.Server
@@ -14,11 +18,12 @@ namespace LWMS.Core.RemoteShell.Server
         int BufferSize;
         //BufferManager
         //SocketAsyncEventArgsPool eventArgsPool;
-        public static string RSAPublicKey;
-        public static string RSAPrivateKey;
+        public static byte[] RSAPublicKey;
+        public static byte[] RSAPrivateKey;
         public Socket Listener;
         Semaphore ThreadLimitation;
-        public RSServer(IPEndPoint point,int MaxConnections,int BufferSize)
+        RSA rsa = RSA.Create();
+        public RSServer(IPEndPoint point, int MaxConnections, int BufferSize)
         {
             Listener = new Socket(point.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
             Listener.Bind(point);
@@ -29,34 +34,144 @@ namespace LWMS.Core.RemoteShell.Server
         bool Stop = false;
         public void Start()
         {
+            rsa.ImportRSAPrivateKey(RSAPrivateKey, out _);
+            rsa.ImportRSAPublicKey(RSAPublicKey, out _);
+            byte[] b = BitConverter.GetBytes(100);
             Listener.Listen(100);
-            Task.Run(() => {
+            Task.Run(() =>
+            {
                 while (Stop is false)
                 {
-                    var s=Listener.Accept();
+                    var s = Listener.Accept();
                     var v = ValidAndLogin(s);
                     if (v.Item1)
                     {
                         ThreadLimitation.WaitOne();
-                        (string, Socket) _s=(v.Item2,s);
+                        AESLayer _s = v.Item2;
                         RSCMDReciver.AddSocket(_s);
                     }
+                    else s.Close();
                 }
             });
         }
         internal void Watch((string, Socket) s)
         {
-            s.Item2.ReceiveTimeout =0;
+            s.Item2.ReceiveTimeout = 0;
         }
-        internal (bool,string) ValidAndLogin(Socket client)
+        internal (bool, AESLayer) ValidAndLogin(Socket client)
         {
-            return (false,"");
+            client.Send(RSAPublicKey);
+            byte[] Key = null;
+            byte[] IV = null;
+            AESLayer layer;
+            {
+                byte[] d0 = new byte[256];
+                client.Receive(d0, 256, SocketFlags.None);
+                var D0 = rsa.Decrypt(d0, RSAEncryptionPadding.OaepSHA256);
+                d0 = new byte[256];
+                client.Receive(Key, 256, SocketFlags.None);
+                var D1 = rsa.Decrypt(d0, RSAEncryptionPadding.OaepSHA256);
+                d0 = new byte[256];
+                client.Receive(d0, 256, SocketFlags.None);
+                IV = rsa.Decrypt(d0, RSAEncryptionPadding.OaepSHA256);
+                List<byte> d = new List<byte>(D0);
+                d.AddRange(D1);
+                Key = d.ToArray();
+                layer = new AESLayer(Key, IV, client);
+                {
+                    byte[] NameD;
+                    byte[] KeyD;
+                    layer.Read(out NameD);
+                    layer.Read(out KeyD);
+                    string UName = Encoding.UTF8.GetString(NameD);
+                    string UKey = Encoding.UTF8.GetString(KeyD);
+                    var auth=OperatorAuthentication.ObtainRTAuth(UName, UKey);
+                    if (OperatorAuthentication.IsAuthPresent(auth))
+                    {
+                        layer.Write(Encoding.UTF8.GetBytes(auth));
+                        return (true, layer);
+                    }
+                    else {return (false, null); }
+                }
+            }
+            return (false, null);
         }
     }
+    class AESLayer:IDisposable
+    {
+        Aes aes = Aes.Create();
+        Socket client;
+        ICryptoTransform Decryptor;
+        ICryptoTransform Encryptor;
+        internal AESLayer(byte[] AESKey, byte[] AESIV, Socket client)
+        {
+            this.client = client;
+            aes.Key = AESKey;
+            aes.IV = AESIV;
+            Decryptor = aes.CreateDecryptor(aes.Key, aes.IV);
+            Encryptor = aes.CreateEncryptor(aes.Key, aes.IV);
+        }
+        public void Write(byte[] data)
+        {
+            {
+                byte[] length = BitConverter.GetBytes(data.Length);
+                var _data = PaddingByte(length, 16);
+                byte[] final = new byte[_data.Length];
+                Encryptor.TransformBlock(_data, 0, _data.Length, final, 0);
+                client.Send(final);
+            }
+            {
+                var _data = PaddingByte(data, 16);
+                byte[] final = new byte[_data.Length];
+                Encryptor.TransformBlock(_data, 0, _data.Length, final, 0);
+                client.Send(final);
+            }
+        }
+        public void Read(out byte[] data)
+        {
+            int length;
+            {
+                byte[] rD = new byte[16];
+                byte[] d = new byte[16];
+                client.Receive(rD, 16, SocketFlags.None);
+                Decryptor.TransformBlock(rD, 0, 16, d, 0);
+                length = BitConverter.ToInt32(new byte[] { d[0], d[1], d[2], d[3] });
+            }
+            {
+                int Padded = ((length / 16) + 1) * 16;
+                byte[] rD = new byte[Padded];
+                byte[] d = new byte[Padded];
+                client.Receive(rD, Padded, SocketFlags.None);
+                Decryptor.TransformBlock(rD, 0, Padded, d, 0);
+                data = new byte[length];
+                for (int i = 0; i < length; i++)
+                {
+                    data[i] = d[i];
+                }
+            }
+        }
+        byte[] PaddingByte(byte[] OriginalData, int Size)
+        {
+            if (OriginalData.Length % Size == 0) return OriginalData;
+            else
+            {
+                int NewSize = ((OriginalData.Length / Size) + 1) * Size;
+                byte[] b = new byte[NewSize];
+                b.Initialize();
+                OriginalData.CopyTo(b, 0);
+                return b;
+            }
+        }
 
+        public void Dispose()
+        {
+            aes.Dispose();
+            client.Dispose();
+        }
+    }
     public class RSCMDReciver : IPipedProcessUnit
     {
-        internal static void AddSocket((string,Socket) s)
+        internal static void AddSocket(AESLayer s)
         {
 
         }
