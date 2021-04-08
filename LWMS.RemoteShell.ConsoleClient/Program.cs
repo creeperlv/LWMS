2fa37e5eb176e1381c01ed0ab287dbd4b998d7f4
Creeper Lv <creeperlv38@outlook.com>
2021-04-08 15:18:08 +0800

    Continue work on the client of RemoteShell.

--- LWMS.RemoteShell.ConsoleClient/Program.cs
+++ LWMS.RemoteShell.ConsoleClient/Program.cs
@@ -1,7 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Net;
+using System.Security.Cryptography;
+using System.Text;
+using LWMS.Core.RemoteShell.ClientCore;
 
 namespace LWMS.RemoteShell.ConsoleClient
 {
@@ -9,7 +13,7 @@ namespace LWMS.RemoteShell.ConsoleClient
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Copyright (C) 2020 Creeper Lv");
+            Console.WriteLine("Copyright (C) 2020-2021 Creeper Lv");
             Console.WriteLine("This software is licensed under the MIT License");
             var path = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
             path = Path.Combine(path, "LWMS.RemoveShell.ConsoleClient");
@@ -20,15 +24,137 @@ namespace LWMS.RemoteShell.ConsoleClient
                 config = SimpleConfig.LoadFromFile(Conf);
             }
             IPEndPoint endPoint;
-            string Address=null;
-            int port=-1;
-            if(config is not null)
+            string Address = null;
+            int port = -1;
+            if (config is not null)
             {
                 Address = config.GetValue("ServerAddress", null);
                 {
-                    int.TryParse(config.GetValue("ServerPort", null), out port);
+                    _ = int.TryParse(config.GetValue("ServerPort", null), out port);
                 }
             }
+            if (Address is null)
+            {
+                Console.WriteLine("Please enter a target server:");
+                Address = Console.ReadLine();
+            }
+            if (port is -1)
+            {
+                Console.WriteLine("Please specify which port to connect:(22 by default)");
+
+                if (int.TryParse(Console.ReadLine(), out port) == false) port = 22;
+            }
+            endPoint = new IPEndPoint(Dns.GetHostAddresses(Address).First(), port);
+            RSClient client = new RSClient(endPoint);
+            var pubKey = client.Handshake00();
+            Console.WriteLine("Received public key from server:");
+            Console.Write(FingerPrint(pubKey));
+            Console.WriteLine("Please make sure if the key is from server.");
+            Console.WriteLine("Enter \"Yes\" to accept the key.");
+            if (Console.ReadLine().ToUpper() == "YES")
+            {
+                Console.WriteLine("Please enter your user name:");
+                var un = Console.ReadLine();
+                Console.WriteLine("Please enter your password:");
+                var pw = ReadPassword();
+                var r = client.Handshake01(un, pw);
+                if (r == true)
+                {
+                    client.RegisterOutput(new ConsoleOut());
+                    while (true)
+                    {
+                        var cmd = Console.ReadLine();
+                        if (cmd.ToUpper() == "EXIT")
+                        {
+                            return;
+                        }
+                        else
+                        {
+                            client.SendOut(cmd);
+                        }
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Rejected by the server.");
+                }
+            }
+            static string ReadPassword()
+            {
+                StringBuilder password = new StringBuilder();
+                ConsoleKeyInfo con;
+
+                do
+                {
+                    con = Console.ReadKey(true);
+                    if (con.Key == ConsoleKey.Backspace)
+                    {
+                        try
+                        {
+                            password.Remove(password.Length - 1, 1);
+                        }
+                        catch (Exception)
+                        {
+                        }
+                    }
+                    else
+                    {
+                        if (con.Key is >= ConsoleKey.D0 and <= ConsoleKey.Z)
+                        {
+                            password.Append(con.KeyChar.ToString());
+                        }
+                    }
+                } while (con.Key != ConsoleKey.Enter);
+                return password.ToString();
+            }
+        }
+        static string FingerPrint(byte[] data)
+        {
+            var d = SHA256.HashData(data);
+            string str = "";
+
+            for (int i = 0; i < d.Length; i++)
+            {
+                var item = d[i];
+                str += string.Format("{0:x2}", item) + " ";
+                if (i == (d.Length / 2) - 1)
+                {
+                    str += "\r\n";
+                }
+            }
+            return str.ToUpper();
+        }
+    }
+    class ConsoleOut : IOutput
+    {
+        public void Flush()
+        {
+            Console.Out.Flush();
+        }
+
+        public void ResetColor()
+        {
+            Console.ResetColor();
+        }
+
+        public void SetBackground(ConsoleColor color)
+        {
+            Console.BackgroundColor = color;
+        }
+
+        public void SetForeground(ConsoleColor color)
+        {
+            Console.ForegroundColor = color;
+        }
+
+        public void Write(string msg)
+        {
+            Console.Write(msg);
+        }
+
+        public void WriteLine(string msg)
+        {
+            Console.WriteLine(msg);
         }
     }
     class SimpleConfig
